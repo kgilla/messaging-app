967b8de0d927c607c3f2b8f8f81b1ab63f271ff1
Kenneth <gilmorekenneth@gmail.com>
2021-02-09 17:48:04 -0500

    Made changes to vars, updated conversation model to allow for more than two participants, and dropped db uri from readme.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Database Information
 
-To connect to the database locally the DB_URI env variable is: "mongodb+srv://admin:kenneth123@cluster0.ts4jw.mongodb.net/messaging-app?retryWrites=true&w=majority". Please don't steal my extremely secure password
+Database URI is in .env variable.
--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const conversationSchema = new Schema({
-  userOne: { type: Schema.Types.ObjectId, ref: "User" },
-  userTwo: { type: Schema.Types.ObjectId, ref: "User" },
+  users: [{ type: Schema.Types.ObjectId, ref: "User" }],
   dateCreated: { type: Date, default: new Date() },
   dateEdited: { type: Date },
   messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
