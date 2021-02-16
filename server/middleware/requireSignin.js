91bad09ab2a680f05d04e272a5e6d66dddfe94fe
Kenneth <gilmorekenneth@gmail.com>
2021-02-16 11:37:15 -0500

    Removed console logs and changed error messaging

--- server/middleware/requireSignin.js
+++ server/middleware/requireSignin.js
@@ -6,7 +6,7 @@ const requireSignin = function (req, res, next) {
       const err = {};
       err.status = 400;
       err.code = "CP_SI_ValidationFailed";
-      err.msg = info.msg;
+      err.error = info.msg;
       return res.json(err);
     }
     req.user = user;
