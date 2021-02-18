a966a020b92df87c465efe81c8e8cb3b1fc76d9a
Kenneth <gilmorekenneth@gmail.com>
2021-02-18 17:55:49 -0500

    Begun rough implementation of socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,23 @@ import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 import { theme } from "./themes/theme";
 import { ProvideAuth } from "./hooks/useAuth";
 import Routes from "./Routes";
+import { io } from "socket.io-client";
 
 function App() {
+  const socket = io("http://localhost:3001");
+
+  socket.on("connect", () => {
+    // either with send()
+    socket.send("Hello!");
+
+    // or with emit() and custom event names
+    socket.emit(
+      "salutations",
+      "Hello!",
+      { mr: "john" },
+      Uint8Array.from([1, 2, 3, 4])
+    );
+  });
   return (
     <MuiThemeProvider theme={theme}>
       <ProvideAuth>
